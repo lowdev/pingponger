5179336a2a370bf56309aac467413b20c9365747
Laury Lafage <lowrey97x@hotmail.fr>
2016-06-12 18:20:05 +0200

    Add 10 arrondissements.

--- app/imports/map/model/mock-tables.ts
+++ app/imports/map/model/mock-tables.ts
@@ -1,6 +1,17 @@
 import { Table } from './Table';
 
 export var TABLE: Table[] = [
-  { lat: 51.673858, lng: 7.815982 },
-  { lat: 51.673858, lng: 7.215982 }
+  { lat: 48.867211, lng: 2.353625, name: "Square Emile Chautemps", address: "" },
+  { lat: 48.8603676, lng: 2.361579699999993, name: "Jardin de l'Hôtel Salé", address: "101 ter, rue des Coutures Saint-Gervais, 75003 Paris" },
+  { lat: 48.864428, lng: 2.360843, name: "Square du Temple", address: "64 Rue de Bretagne, 75003 Paris" },
+  { lat: 48.853862, lng: 2.358016, name: "Jardin de la Cité Internationale des Arts", address: "18 Rue de l'Hôtel de ville, 75003 Paris" },
+  { lat: 48.858733, lng: 2.357916, name: "Square Charles-Victor Langlois", address: "6 Rue des Blancs Manteaux, 75004 Paris" },
+  { lat: 48.838807, lng: 2.353810, name: "Square Scipion", address: "23 Rue du Fer À Moulin, 75005 Paris" },
+  { lat: 48.847616, lng: 2.360667, name: "Jardin Tino-Rossi", address: "Quai Saint-Bernard, 75005 Paris" },
+  { lat: 48.843050, lng: 2.336944, name: "Jardin Marco Polo", address: "Jardin des Grands Explorateurs, 75006 Paris" },
+  { lat: 48.843050, lng: 2.336944, name: "Jardin Robert Cavelier de la Salle", address: "Jardin des Grands Explorateurs, 75006 Paris" },
+  { lat: 48.850033, lng: 2.312919, name: "", address: "40 Avenue Duquesne, 75007 Paris" },
+  { lat: 48.875939, lng: 2.320600, name: "Square Marcel Pagnol", address: "Square Marcel Pagnol, 75008 Paris" },
+  { lat: 48.876701, lng: 2.331649, name: "", address: "2 rue de la trinité, 75009 Paris" },
+  { lat: 448.877079, lng: 2.346531, name: "Square Montholon", address: "Rue de Montholon, 75009 Paris" }
 ];
